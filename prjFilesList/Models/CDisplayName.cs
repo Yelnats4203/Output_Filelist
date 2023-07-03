69a734e09ae88603867525b6477abcdbd9c2a9a6
Stanley <stanleylin@uitctech.com.tw>
2023-07-03 15:24:43 +0800

    refactor:use display attribute as list header
    以File類別的屬性名稱的display attribute作為清單的header
    新增資料夾Models存放類別

---
+++ prjFilesList/Models/CDisplayName.cs
@@ -0,0 +1,31 @@
+﻿using NPOI.HPSF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjFilesList.Models
+{
+    public static class CDisplayName
+    {
+        public static string? GetDisplayName(this object instance, string propertyName)
+        {
+            //get property
+            PropertyInfo? propertyInfo = instance.GetType().GetProperty(propertyName);
+            if (propertyInfo == null)
+                return null;
+
+            //get property's display attribute
+            DisplayAttribute? displayAttribute = propertyInfo.GetCustomAttribute<DisplayAttribute>();
+            if (displayAttribute == null)
+                return null;
+
+            return displayAttribute.Name;
+        }
+
+    }
+}
--- prjFilesList/Program.cs
+++ prjFilesList/Program.cs
@@ -1,4 +1,7 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using prjFilesList.Models;
 
 namespace prjFilesList
 {
@@ -6,12 +9,13 @@ namespace prjFilesList
     {
         static void Main(string[] args)
         {
-            List<string> sheetNames = new List<string>{ "ADMIN", "API", "BATCH", "SFTP" };
+            List<string> sheetNames = new List<string> { "ADMIN", "API", "BATCH", "SFTP" };
             string folderPath = @"D:\作業\進管程式清單小作業\進管\";
             string targetPath = @"S:\進管\";
             CCreateFileList fileList = new CCreateFileList(sheetNames, folderPath, targetPath);
-            fileList.CreateDataList();
-            fileList.CreateListFile();
+            fileList.Run();
+
+            Console.WriteLine("清單生成完成。");
         }
     }
 }
